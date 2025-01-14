82ad845b6ce46b66c109b5771890e4d8c283b9e1
killsime <killsime@qq.com>
2025-01-14 16:47:42 +0800

    使用tailwindcss，解析markdown语法

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,49 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 const Blog = () => {
-    return <h1>这是博客页面</h1>;
+    const [markdownContent, setMarkdownContent] = useState<string>('');
+
+    useEffect(() => {
+        const fetchMarkdown = async () => {
+            const response = await fetch('https://raw.githubusercontent.com/killsime/my-blog/main/README.md');
+            const text = await response.text();
+            setMarkdownContent(text);
+        };
+
+        fetchMarkdown();
+    }, []);
+
+    return (
+        <div className="prose lg:prose-xl">
+            <h1>这是博客页面</h1>
+            <ReactMarkdown
+                children={markdownContent}
+                components={{
+                    code({ node, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || '');
+                        return match ? (
+                            <SyntaxHighlighter
+                                style={dracula}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}
+                            >
+                                {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        );
+                    }
+                }}
+            />
+        </div>
+    );
 };
 
 export default Blog;
